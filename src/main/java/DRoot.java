0f5cbcdd4debfb8cc4fd2ac020c85b5406f6a95f
rbulin <roman.bulinski@pentacomp.pl>
2020-01-11 09:33:29 +0100

    Refactor code

--- src/main/java/DRoot.java
+++ src/main/java/DRoot.java
@@ -1,18 +1,16 @@
+import java.util.Arrays;
+
 public class DRoot {
 
     public static int digital_root(int n) {
-        int sum = getSum2(n);
-        int sum2 = getSum2(sum);
-        return sum2;
+        int sum = createSumFormNumber(n);
+        int secondSum = createSumFormNumber(sum);
+        return secondSum;
     }
 
-    private static int getSum2(int sum) {
-        String st2 = String.valueOf(sum);
-        String[] stnums2 = st2.split("");
-        int sum2 = 0;
-        for (int i = 0; i < stnums2.length; i++) {
-            sum2 = sum2 + Integer.valueOf(stnums2[i]);
-        }
-        return sum2;
+    private static int createSumFormNumber(int sum) {
+        String[] numbers = String.valueOf(sum).split("");
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
     }
+
 }
